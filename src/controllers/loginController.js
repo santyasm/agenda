4b861944c9eefac972fb24016ef0487334094c14
Yasmin Santana <94482549+santyasm@users.noreply.github.com>
2023-05-07 11:25:59 -0300

    confirmpass

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,8 +5,14 @@ export default class LoginController{
 		res.render('login/index');
 	}
 	static async store(req, res) {
-		const { email, name, password } = req.body;
+		const { email, name, password, confirmpass } = req.body;
 		try {
+
+			if (confirmpass != password) {
+				console.log('Senhas diferentes.');
+				return res.redirect('/login/index');
+			} 
+			
 			const newUser = await User.create({ email, name, password });
             
 			res.redirect('/');
