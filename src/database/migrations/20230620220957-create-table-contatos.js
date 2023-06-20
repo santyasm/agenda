51f87a5c90276edf3ecba1e231ca3969e3705d59
santyasm <yyasant5@gmail.com>
2023-06-20 19:35:37 -0300

    contacts

--- src/database/migrations/20230620220957-create-table-contatos.js
+++ src/database/migrations/20230620220957-create-table-contatos.js
@@ -3,20 +3,53 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+   
+    await queryInterface.createTable('contacts', 
+      {
+       id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true, 
+        allowNull: false,
+        primaryKey: true
+      }, 
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      telephone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      birthday: {
+        type: Sequelize.DATE,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id"
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE"
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      },
+      );
+     
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+     await queryInterface.dropTable('users');
+     
   }
 };
